85c11e8d18c40f536b805f7f4222ab424f19ee85
osierosavo <osierosavo@gmail.com>
2023-03-23 15:07:11 +0900

    add swiper

--- style.js
+++ style.js
@@ -29,3 +29,24 @@ const mSearch = document.querySelector(".mSearch");
 mSearchBtn.addEventListener('click', function(){
   mSearch.classList.toggle('active');
 });
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
